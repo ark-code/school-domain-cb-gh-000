51831f0a019e47543076404f6b43cceb13888c85
Akshay K <akshaykunti1@gmail.com>
2020-01-15 04:30:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School 
+  
+
+  def initialize(shool_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def add_student(name, id)
+    @name = name
+    @id = id 
+    @roster[id] = []
+    @roster[id] << name
+  end
+  
+  def roster 
+    @roster
+  end
+  
+  
+end
